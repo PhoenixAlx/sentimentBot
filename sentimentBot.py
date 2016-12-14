301684509c6324fed7c4a9dce033e2a2b7590172
PhoenixAlx <raskalakabra@gmail.com>
2016-12-14 16:33:19 +0000

    create python

--- sentimentBot.py
+++ sentimentBot.py
@@ -1,105 +1,83 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Simple Bot to reply to Telegram messages
-# This program is dedicated to the public domain under the CC0 license.
-"""
-This Bot uses the Updater class to handle the bot.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic inline bot example. Applies different text transformations.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-from uuid import uuid4
-
-import re
-
-from telegram import InlineQueryResultArticle, ParseMode, \
-    InputTextMessageContent
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+# This example show how to write an inline mode telegramt bot use pyTelegramBotAPI.
+import telebot
+import time
+import sys
 import logging
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
+from telebot import types
+import rpy2.robjects as ro
+from rpy2.robjects.packages import importr
 
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-    update.message.reply_text('Hi!')
+API_TOKEN = 'Yourtoken'
+
+bot = telebot.TeleBot(API_TOKEN)
+telebot.logger.setLevel(logging.DEBUG)
+extremes = importr('syuzhet')
+codigo_r_library = """
+libreryLoad <- function(name) {
+    requiredPackages = c(name)
+    for(p in requiredPackages){
+      if(!require(p,character.only = TRUE)) install.packages(p,repos="http://cran.rstudio.com/")
+      library(p,character.only = TRUE)
+    }
+}
+"""
+ro.r(codigo_r_library);
+codigo_r = """
+resultSent <- function(cadena) {
+  vectorS<-get_nrc_sentiment(cadena);
+  anger<-paste("Anger: ",vectorS$anger);
+  anticipation<-paste("Anticipation: ",vectorS$anticipation);
+  disgust<-paste("Disgust: ",vectorS$disgust);
+  fear<-paste("Fear: ",vectorS$fear);
+  joy<-paste("Joy: ",vectorS$joy);
+  sadness<-paste("Sadness: ",vectorS$sadness);
+  surprise<-paste("Surprise: ",vectorS$surprise);
+  trust<-paste("Trust: ",vectorS$trust);
+  negative<-paste("Negative: ",vectorS$negative);
+  positive<-paste("Positive: ",vectorS$positive);
+  totalResult<-paste(positive,negative,trust,surprise,sadness,joy,fear,disgust,anticipation,anger,sep="\n")
+  return(totalResult)
+}
+"""
+ro.r(codigo_r);
+loadLib = ro.globalenv['libreryLoad'];
+loadLib('syuzhet');
+resultSent_py = ro.globalenv['resultSent'];
 
 
-def help(bot, update):
-    update.message.reply_text('Help!')
+@bot.inline_handler(lambda query: query.query != '')
+def query_text(inline_query):
+    try:
+        res = resultSent_py(inline_query.query);
+        textOut=res[0];
+        r = types.InlineQueryResultArticle('1', 'View score', types.InputTextMessageContent(textOut))
+        
+        bot.answer_inline_query(inline_query.id, [r])
+    except Exception as e:
+        print(e)
 
 
-def escape_markdown(text):
-    """Helper function to escape telegram markup symbols"""
-    escape_chars = '\*_`\['
-    return re.sub(r'([%s])' % escape_chars, r'\\\1', text)
 
 
-def inlinequery(bot, update):
-    query = update.inline_query.query
-    results = list()
-
-    results.append(InlineQueryResultArticle(id=uuid4(),
-                                            title="Caps",
-                                            input_message_content=InputTextMessageContent(
-                                                query.upper())))
-
-    results.append(InlineQueryResultArticle(id=uuid4(),
-                                            title="Bold",
-                                            input_message_content=InputTextMessageContent(
-                                                "*%s*" % escape_markdown(query),
-                                                parse_mode=ParseMode.MARKDOWN)))
-
-    results.append(InlineQueryResultArticle(id=uuid4(),
-                                            title="Italic",
-                                            input_message_content=InputTextMessageContent(
-                                                "_%s_" % escape_markdown(query),
-                                                parse_mode=ParseMode.MARKDOWN)))
-
-    update.inline_query.answer(results)
+#bot.inline_handler(lambda query: len(query.query) is 0)
+def default_query(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'waiting', types.InputTextMessageContent('waiting'))
+        bot.answer_inline_query(inline_query.id, [r])
+    except Exception as e:
+        print(e)
 
 
-def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
-
-
-def main():
-    # Create the Updater and pass it your bot's token.
-    updater = Updater("TOKEN")
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(InlineQueryHandler(inlinequery))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Block until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+def main_loop():
+    bot.polling(True)
+    while 1:
+        time.sleep(3)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        print >> sys.stderr, '\nExiting by user request.\n'
+        sys.exit(0)
